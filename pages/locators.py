b8c631635d0bb73a6d522053fb7e5070ae087000
Dmitrii Lukashev <seo@chillicode.ru>
2021-05-31 18:20:11 +0300

    решение этапа 4.3.3, поиск и сравнение названия товара и цены

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,8 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTRATION_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    BUTTON_PAGE = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRODUCT_TITLE = (By.CSS_SELECTOR, "h1")
+    ALERT_PRODUCT_NAME = (By.CSS_SELECTOR, "#massage .alertinner")
